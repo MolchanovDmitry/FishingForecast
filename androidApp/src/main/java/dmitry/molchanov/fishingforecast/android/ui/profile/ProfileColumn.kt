6eafd7784e3a516cc0f852067f7c2d12dea563e5
d.molchanov@nut.tech <12draPj8>
2022-07-13 21:03:32 +0300

    Подтверждение удаления профиля

--- androidApp/src/main/java/dmitry/molchanov/fishingforecast/android/ui/profile/ProfileColumn.kt
+++ androidApp/src/main/java/dmitry/molchanov/fishingforecast/android/ui/profile/ProfileColumn.kt
@@ -1,17 +1,13 @@
 package dmitry.molchanov.fishingforecast.android.ui.profile
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +15,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dmitry.molchanov.fishingforecast.android.R
+import dmitry.molchanov.fishingforecast.android.ui.common.BaseDialog
 import dmitry.molchanov.fishingforecast.model.Profile
 
 @Composable
@@ -36,48 +33,60 @@ fun ProfileColumn(
         }
         mutableStateOf(profiles[defaultIndex])
     }
-
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
-        items(profiles) { profile ->
-            Row(
-                modifier = Modifier
-                    .padding(all = 16.dp)
-                    .padding(end = 4.dp)
-                    .clickable {
-                        selectedOption.value = profile
-                        onOptionSelected(profile)
-                    },
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                RadioButton(
-                    selected = (profile == selectedOption.value),
-                    colors = RadioButtonDefaults.colors(
-                        selectedColor = MaterialTheme.colors.primary,
-                        unselectedColor = MaterialTheme.colors.primary
-                    ),
-                    onClick = null
-                )
-                Text(
-                    text = if (profile.isCommon) stringResource(R.string.common_profile) else profile.name,
-                    fontSize = 18.sp,
-                    modifier = Modifier
-                        .weight(1F)
-                        .padding(8.dp)
-                )
-                if (!profile.isCommon) {
-                    Icon(
-                        imageVector = Icons.Filled.Delete,
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.primary,
-                        modifier = Modifier.clickable { deleteOption(profile) }
-                    )
-                }
-            }
-            Divider(
-                color = Color.LightGray,
-                thickness = 1.dp,
-                modifier = Modifier.padding(start = 16.dp)
+    var profileToDelete by remember { mutableStateOf<Profile?>(null) }
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (profileToDelete != null) {
+            BaseDialog(
+                message = "Удалить профиль ${profileToDelete?.name} ?",
+                onPositive = {
+                    profileToDelete?.let { deleteOption(it) }
+                    profileToDelete = null
+                },
+                onNegative = { profileToDelete = null }
             )
+        }
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            items(profiles) { profile ->
+                Row(
+                    modifier = Modifier
+                        .padding(all = 16.dp)
+                        .padding(end = 4.dp)
+                        .clickable {
+                            selectedOption.value = profile
+                            onOptionSelected(profile)
+                        },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (profile == selectedOption.value),
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = MaterialTheme.colors.primary,
+                            unselectedColor = MaterialTheme.colors.primary
+                        ),
+                        onClick = null
+                    )
+                    Text(
+                        text = if (profile.isCommon) stringResource(R.string.common_profile) else profile.name,
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .weight(1F)
+                            .padding(8.dp)
+                    )
+                    if (!profile.isCommon) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.primary,
+                            modifier = Modifier.clickable { profileToDelete = profile }
+                        )
+                    }
+                }
+                Divider(
+                    color = Color.LightGray,
+                    thickness = 1.dp,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
         }
     }
 }
