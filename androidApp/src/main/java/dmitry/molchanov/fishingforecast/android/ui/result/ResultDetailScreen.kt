f9cb156b0cdad29d62f5680b364545b606af1c5d
d.molchanov@nut.tech <12draPj8>
2022-07-24 11:27:57 +0300

    Печать информации о погоде за выбранную дату

--- androidApp/src/main/java/dmitry/molchanov/fishingforecast/android/ui/result/ResultDetailScreen.kt
+++ androidApp/src/main/java/dmitry/molchanov/fishingforecast/android/ui/result/ResultDetailScreen.kt
@@ -1,7 +1,11 @@
 package dmitry.molchanov.fishingforecast.android.ui.result
 
+import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,6 +27,9 @@ import dmitry.molchanov.fishingforecast.model.Result
 import dmitry.molchanov.fishingforecast.model.WeatherData
 import dmitry.molchanov.fishingforecast.utils.getDayCount
 import org.koin.androidx.compose.viewModel
+import java.util.*
+
+private val UNKNOWN = "Нет данных"
 
 @Preview
 @Composable
@@ -32,9 +39,9 @@ fun ResultDetailScreen(
 ) {
     val vm by viewModel<ResultDetailViewModel>()
     val state = vm.stateFlow.collectAsState()
-    val singapore = LatLng(result.mapPoint.latitude, result.mapPoint.longitude)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 12f)
+        val point = LatLng(result.mapPoint.latitude, result.mapPoint.longitude)
+        position = CameraPosition.fromLatLngZoom(point, 12f)
     }
     val sortedWeatherData = remember {
         val sorted = weatherData.sortedBy { it.date }
@@ -42,7 +49,7 @@ fun ResultDetailScreen(
             ?.let { vm.onAction(OnDateSelected(it.date)) }
         sorted
     }
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
         Box(modifier = Modifier.fillMaxWidth().height(200.dp)) {
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
@@ -72,7 +79,72 @@ fun ResultDetailScreen(
                 }
             }
         }
-        Text(modifier = Modifier.fillMaxWidth(), text = "asdasdad")
+        weatherData.firstOrNull {
+            it.date == state.value.selectedDate
+        }?.let { selectedWeatherDataItem -> ResultDetailColumn(selectedWeatherDataItem) }
+
     }
 
+}
+
+@Composable
+private fun ResultDetailColumn(weatherDateItem: WeatherData) {
+    val simpleDateFormat = remember { SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()) }
+    val dataStr = simpleDateFormat.format(weatherDateItem.date)
+
+    ResultDetailItemRow(title = "Дата", value = dataStr)
+
+    ResultDetailItemRow(
+        title = "Средняя температура окружающей среды",
+        value = weatherDateItem.temperature?.avg?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Минимальная температура окражающей среды",
+        value = weatherDateItem.temperature?.min?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Максимальная температура окружающей среды",
+        value = weatherDateItem.temperature?.max?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Температура воды",
+        value = weatherDateItem.temperature?.water?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Влажность",
+        value = weatherDateItem.humidity?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Давление в мм ртутного столба",
+        value = weatherDateItem.pressure?.mm?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Давление в паскалях",
+        value = weatherDateItem.pressure?.pa?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Направление ветра",
+        value = weatherDateItem.wind?.dir ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Порыв ветра",
+        value = weatherDateItem.wind?.gust?.toString() ?: UNKNOWN
+    )
+    ResultDetailItemRow(
+        title = "Скорость ветра",
+        value = weatherDateItem.wind?.speed?.toString() ?: UNKNOWN
+    )
+
+}
+
+@Composable
+private fun ResultDetailItemRow(title: String, value: String, showDivider: Boolean = true) {
+    Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 16.dp)) {
+        Text(text = title)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = value)
+    }
+    if (showDivider) {
+        Divider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.padding(horizontal = 16.dp))
+    }
 }
